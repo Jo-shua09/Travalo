07e496066eb542057a9422d6124026701c4d388b
Jo-shua09 <joshuaonyeka039@gmail.com>
2026-05-10 19:53:35 -0700

    refactor: add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 8080,
   },
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
